c46cb88953c90b31a104a0478db277a1fd20d82f
deliodotexe <lionardo.wernli@gmail.com>
2023-12-28 15:47:45 +0100

    created basic response of backend without any data yet.

--- backend.php
+++ backend.php
@@ -1,1 +1,14 @@
-<?php?>
+<?php?
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //Handling Get request from FetchData
+
+    //Zusatztechniken 2: some SQL request to grab data from DB and return it
+}
+
+
+
+
+
+
+>
